3a6c1154b87dd000595fbdf9c831b17911b77ee6
Nicholas Hallahan <nick@theoutpost.io>
2015-12-21 10:36:54 -0800

    scaffolding #9

---
+++ odk/middlewares/aggregate-osm.js
@@ -0,0 +1,14 @@
+/**
+ * aggregate-osm.js
+ *
+ * Aggregates a list of JSOM formatted OSM edit files into a single OSM XML.
+ * A filter can be applied to get a subset of the OSM edits in question.
+ * The resultant single OSM is fed to the callback.
+ *
+ * @param files   - OSM Files to aggregate
+ * @param filter  - Object that specifies elements to include - no filter if null
+ * @param cb      - Final OSM XML, as well as progress.
+ */
+module.exports = function (files, filter, cb) {
+
+};
