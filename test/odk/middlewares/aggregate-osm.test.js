cfa5a8b348c43f3c51563b02420780583bd46cc5
Nicholas Hallahan <nick@theoutpost.io>
2015-12-22 10:46:51 -0800

    yay, unit tests 	#9

--- test/odk/middlewares/aggregate-osm.test.js
+++ test/odk/middlewares/aggregate-osm.test.js
@@ -1,3 +1,20 @@
 const test = require('tape');
+const fs = require('fs');
 const aggregateOsm = require('../../../odk/middlewares/aggregate-osm');
 
+const simpleFiles = fs.readdirSync('./test/fixtures/osm/basic');
+const simpleFilesPaths = [];
+for (var i = 0, len = simpleFiles.length; i < len; i++) {
+    var fName = simpleFiles[i];
+    simpleFilesPaths.push('./test/fixtures/osm/basic/' + fName);
+}
+
+test('Check to see aggregate-osm concatenate 3 files and rewrite a negative ID.', function (t) {
+    t.plan(2);
+    aggregateOsm(simpleFilesPaths, null, function (err, xml) {
+        t.error(err, 'Should not throw error.');
+        const nodeWithNegId = '<node id="-1" action="modify" lat="1.0753199144383814" lon="34.16216178888775">';
+        const idxRetVal = xml.indexOf(nodeWithNegId);
+        t.ok(idxRetVal > 0, 'there is a node with a -1 id');
+    });
+});
