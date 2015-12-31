a25843e5fb10d0722ade1f316b9b5d8437a9abba
Nicholas Hallahan <nick@theoutpost.io>
2015-12-31 12:28:34 -0800

    fixed upload form paths since refactor

--- odk/controllers/upload-form.js
+++ odk/controllers/upload-form.js
@@ -42,7 +42,7 @@ module.exports = function (req, res, next) {
             var xFormFilename = xlsFilename.replace('.xlsx', '.xml');
             var xFormPath = formsDir + xFormFilename;
             var options = {
-                scriptPath: __dirname + '/../../pyxform/pyxform/',
+                scriptPath: __dirname + '/../pyxform/pyxform/',
                 args: [xlsPath, xFormPath],
                 mode: "text"
             };
