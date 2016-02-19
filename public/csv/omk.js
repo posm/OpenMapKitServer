6a4dbd0e704b474d2c1f586928646d773c16b428
Nicholas Hallahan <nick@theoutpost.io>
2016-02-18 17:32:11 -0800

    automatically fetches OMK ODK JSON from query param #22

---
+++ public/csv/omk.js
@@ -0,0 +1,37 @@
+window.OMK = {};
+
+OMK.fetch = function () {
+    OMK.fetchJSON(getParam('json'));
+
+    var form = getParam('form');
+    if (form) {
+        var url = OMK.omkServerUrl() + '/submissions/' + form + '.json';
+        OMK.fetchJSON(url);
+    }
+
+};
+
+OMK.fetchJSON = function (url) {
+    if (!url) return;
+
+    $.get(url, function(data, status, xhr) {
+        $(".json textarea").val(JSON.stringify(data));
+        doJSON();
+        showJSON(true);
+    }).fail(function(xhr, status, errorThrown) {
+        console.log("Error fetching ODK submissions!");
+        console.log(xhr);
+        console.log(status);
+        console.log(errorThrown);
+    });
+};
+
+/**
+ * Determines the OMK Server endpoint.
+ *
+ * @returns {*}
+ */
+OMK.omkServerUrl = function () {
+    var omkServer = getParam('omk_server');
+    return (omkServer ? omkServer : window.location.origin);
+};
