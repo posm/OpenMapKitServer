c6f744bbf6aedc40d9663b117afcd1cad7972e28
Rene Rodriguez <rrlara@gmail.com>
2016-03-04 11:03:24 -0800

    Done. migrating Jekyll to Pages.

---
+++ pages/js/assets/omk.js
@@ -0,0 +1,47 @@
+window.OMK = {};
+
+OMK.fetch = function () {
+    OMK.fetchJSON(OMK.jsonUrl());
+};
+
+OMK.jsonUrl = function () {
+    var json = getParam('json');
+    if (!json) {
+        var form = getParam('form');
+        if (form) {
+            $('h1').html(capitalizeFirstLetter(form.replace(/_/g,' ')));
+            //json = OMK.omkServerUrl() + '/omk/pages/submissions/' + form + '.json';
+            json = 'http://localhost:3210/omk/odk/submissions/' + form + '.json';
+        }
+    }
+    return json;
+};
+
+//Function to capitalise first character for strings
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+OMK.fetchJSON = function (url) {
+    if (!url) return;
+
+    $.get(url, function(data, status, xhr) {
+        $('.fetching').hide();
+        doCSV(data);
+    }).fail(function(xhr, status, errorThrown) {
+        console.log("Error fetching ODK submissions!");
+        console.log(xhr);
+        console.log(status);
+        console.log(errorThrown);
+    });
+};
+
+/**
+ * Determines the OMK Server endpoint.
+ *
+ * @returns {*}
+ */
+OMK.omkServerUrl = function () {
+    var omkServer = getParam('omk_server');
+    return (omkServer ? omkServer : window.location.origin);
+};
--- pages/submissions/README.md
+++
@@ -1,5 +0,0 @@
-# CSV
-
-We have modified http://konklone.io/json/ to specifically consume JSON from OpenMapKit Server endpoints.
-
-[Original source](https://github.com/konklone/json) can be found on Github. It's a public domain project.
