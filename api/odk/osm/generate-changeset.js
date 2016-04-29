ba842bb40cebfe50f2fef7ffa64be1012a310293
Nicholas Hallahan <nick@theoutpost.io>
2016-04-29 08:43:04 -0700

    #13 #63 We have all of the methods figured out in xmlbuilder

--- api/odk/osm/generate-changeset.js
+++ api/odk/osm/generate-changeset.js
@@ -7,10 +7,11 @@ module.exports = function (submissionsDir) {
     var createdBy = 'OpenMapKit Server v' + pkg.version;
     var omkPath = '';
 
-    return builder.create('root', {version: '1.0', encoding: 'UTF-8'})
-        .ele('osm', {version: 0.6})
-            .ele('changeset')
-                .ele('tag', {k: 'testkey', v: 'testval'})
+    return builder.create('osm', {version: '1.0', encoding: 'UTF-8'})
+        .att({version: '0.6'})
+        .ele('changeset')
+            .ele('tag', {k: 'testkey', v: 'testval'})
+            .insertAfter('tag', {k: 'testkey2', v: 'testval2'})
         .end({ pretty: true});
 
 };
