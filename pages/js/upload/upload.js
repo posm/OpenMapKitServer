dc9141052102471d6ae08910df91a8451a5e2f5e
Seth Fitzsimmons <seth@mojodna.net>
2017-03-27 21:33:14 -0700

    Missing JS tweaks for 694bf1469a970c657ee222333743603040e2658f

--- pages/js/upload/upload.js
+++ pages/js/upload/upload.js
@@ -182,7 +182,7 @@ new Vue({
             console.log('onFormError', el, err);
             // indicate the changes
             //Failed message
-            this.uploadMessage = "Failed uploading <code>" + this.fileName + "</code>: " + err.msg;
+            this.uploadMessage = "Failed uploading " + this.fileName + ": " + err.msg;
             this.response = err;
 
             //toaster
