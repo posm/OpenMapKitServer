f9956acb5e24f94a7a2114edacaa8f421d70da84
Wille Marcel <wille@wille.blog.br>
2018-02-15 09:41:40 -0200

    show upload form only to admin users

--- jekyll/js/admindv/admindv.js
+++ jekyll/js/admindv/admindv.js
@@ -2,6 +2,7 @@ new Vue({
   el: '.admin-dv-restricted',
   data: {
     adminDVPermission: false,
+    role: null,
   },
   ready: function() {
     this.getAuthentication();
@@ -14,8 +15,10 @@ new Vue({
             'adminDVPermission',
             ['admin', 'data-viewer'].includes(response.data.role)
           );
+          this.$set('role', response.data.role);
         } else {
           this.$set('adminDVPermission', false);
+          this.$set('role', null);
         }
       });
     }
