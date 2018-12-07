f2feea2946d1fcf9370ad4b3e3118df415aa308c
Wille Marcel <wille@wille.blog.br>
2018-12-07 07:28:48 -0300

    add --delete option to s3 sync command

--- util/get_data_files.js
+++ util/get_data_files.js
@@ -5,7 +5,7 @@ var child;
 const AWSBUCKETPREFIX = process.env.AWSBUCKETPREFIX ? process.env.AWSBUCKETPREFIX : '/';
 
 child = exec(
-  `aws s3 sync s3://${process.env.AWSBUCKETNAME}/${AWSBUCKETPREFIX}/ ${settings.dataDir}`,
+  `aws s3 sync s3://${process.env.AWSBUCKETNAME}/${AWSBUCKETPREFIX}/ ${settings.dataDir} --delete`,
   {maxBuffer: 1024 * 5000},
   function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
