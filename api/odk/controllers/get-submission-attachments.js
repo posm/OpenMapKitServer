2fa8907a878558eadffb19e1e828ce4a781d19f4
Dakota Benjamin <dakota.benjamin@hotosm.org>
2019-01-29 14:27:34 -0500

    Filter attachments

--- api/odk/controllers/get-submission-attachments.js
+++ api/odk/controllers/get-submission-attachments.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const archiver = require('archiver');
 const recursive = require('recursive-readdir');
-
+const aggregate = require('../helpers/aggregate-submissions');
 const settings = require('../../../settings');
 
 /**
@@ -14,7 +14,32 @@ module.exports = (req, res, next) => {
   const formName = req.params.formName;
   const submissionsDir = path.join(settings.dataDir, 'submissions', formName);
 
-  return recursive(submissionsDir, (err, files) => {
+  // returns a list of folder IDs that comply with the filter
+  function getFilterIds() {
+    return aggregate({
+      formName: req.params.formName,
+      limit: req.query.limit,
+      offset: req.query.offset,
+      startDate: req.query.start_date,
+      endDate: req.query.end_date,
+      deviceId: req.query.deviceId,
+      username: req.query.username
+    }, (err, aggregate) => {
+      if (err) {
+        console.warn(err.stack);
+        return 0;
+      }
+
+      var folders = [];
+
+      console.log(aggregate[0]['meta']['instanceId'].split(':')[1]);
+
+      aggregate.forEach(function(element) {folders.push(element['meta']['instanceId'].split(':')[1]);});
+      return folders;
+    });
+  }
+
+  return recursive(submissionsDir, ['data.json', 'data.xml', '*.osm'], (err, files) => {
     if (err) {
       return res.status(500).json({
         status: 500,
@@ -40,7 +65,16 @@ module.exports = (req, res, next) => {
 
     archive.pipe(res);
 
+    var filterList = getFilterIds()
+    // console.dir(filterList);
+
     files
+      // includes only folders in the filterList 
+      .filter(filename => {
+        var fileSplit = filename.split('/');
+        var fileId = fileSplit[fileSplit.length - 2];
+        return filterList.indexOf(fileId) >= 0;
+      })
       // ignore OSM files
       .filter(filename => path.extname(filename) !== '.osm')
       // ignore XForm responses
